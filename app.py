0f64c9fe0e50e1c9a9ba0619bad4b330293a4273
prathmeshp290605-png <prathmeshp290605@gmail.com>
2026-08-19 15:19:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ if uploaded_file is not None:
     type=["jpg", "jpeg", "png"]
 )
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+
     st.image(image, caption="Uploaded Image", use_container_width=True)
 if st.button("Detect License Plate"):
         st.info("Detecting license plate...")
